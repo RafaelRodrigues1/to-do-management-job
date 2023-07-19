dce63d57aa194193d3b50d8f7b7d84bf8665542b
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2023-07-19 19:15:44 -0300

    Scheduler para envio de usuários e mensagens de email para fila

--- src/main/kotlin/com/personalproject/todomanagementjob/scheduler/UserScheduler.kt
+++ src/main/kotlin/com/personalproject/todomanagementjob/scheduler/UserScheduler.kt
@@ -1,7 +1,14 @@
 package com.personalproject.todomanagementjob.scheduler
 
+import com.google.gson.GsonBuilder
+import com.personalproject.todomanagementjob.business.EmailBusiness
 import com.personalproject.todomanagementjob.mapper.UserMapper
+import com.personalproject.todomanagementjob.model.Email
+import com.personalproject.todomanagementjob.model.User
+import com.personalproject.todomanagementjob.model.UserStatus
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
@@ -12,8 +19,35 @@ class UserScheduler {
     @Autowired
     private lateinit var userMapper: UserMapper
 
-    @Scheduled(fixedDelay = 3, timeUnit = TimeUnit.SECONDS)
+    @Autowired
+    private lateinit var emailBusiness: EmailBusiness
+
+    @Autowired
+    private lateinit var rabbitTemplate: RabbitTemplate
+
+    @Value("\${queue.envio-solicitacao-confirmacao-cadastro}")
+    private lateinit var envioSolicitacaoConfirmacaoCadastroQueue: String
+
+    @Value("\${queue.envio-confirmacao-cadastro}")
+    private lateinit var envioConfirmacaoCadastroQueue: String
+
+    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.SECONDS)
     fun getusers() {
-        println(userMapper.selectAllUsers().stream().map{user -> user.name}.reduce{a, b -> "$a, $b"}.get())
+        val listusersPendentes: List<User> = userMapper.findAllUsersByStatus(UserStatus.PENDENTE)
+        if(!listusersPendentes.isNullOrEmpty())
+            listusersPendentes.forEach{ user -> processaEnvioSolicitacaoConfirmacaoUsuario(user) }
+        else
+            println("Não há usuários para solicitação de confirmação de cadastro!")
+    }
+
+    private fun processaEnvioSolicitacaoConfirmacaoUsuario(user: User) {
+        val email: Email = emailBusiness.buildEmailSolicitacaoConfirmacaoCadastro()
+        email.receiver = user
+        enviaParaFilaEmail(email, envioSolicitacaoConfirmacaoCadastroQueue)
+        userMapper.updateStatusUser(user.id.toString(), UserStatus.PROCESSANDO)
+    }
+
+    private fun enviaParaFilaEmail(email: Email, queueName: String) {
+        rabbitTemplate.convertAndSend(queueName, GsonBuilder().create().toJson(email))
     }
 }
